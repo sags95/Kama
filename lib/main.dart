c49a28b8190afb5c271276a1b34731e08694c60a
sags95 <alex.sagel7587@gmail.com>
2019-09-04 17:22:13 -0400

    conditional routing based on auth state

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todoey_flutter/models/task_data.dart';
-import 'package:todoey_flutter/screens/reg_screen.dart';
 import 'package:todoey_flutter/screens/tasks_screen.dart';
 import 'package:todoey_flutter/screens/welcome_screen.dart';
-import 'package:todoey_flutter/screens/login_screen.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main () => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+//TODO: get auth status and render proper screen
+
+class MyApp extends StatefulWidget {
+
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isUser = false;
+
+  @override
+   initState() {
+     FirebaseAuth.instance.currentUser().then((curUser){
+       if (curUser != null){
+         isUser = true;
+         Navigator.pushReplacementNamed(context, TasksScreen.id);
+       } else {
+         Navigator.pushReplacementNamed(context, WelcomeScreen.id);
+       }
+     });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider( //root provider
@@ -22,12 +44,10 @@ class MyApp extends StatelessWidget {
           accentColor: Color(0xff6200EE),
           primaryColor: Color(0xff8a00ee),
         ),
-        initialRoute: WelcomeScreen.id,
+        initialRoute: _MyAppState().isUser ? TasksScreen.id : WelcomeScreen.id,
         routes: {
           TasksScreen.id: (context) => TasksScreen(),
           WelcomeScreen.id: (context) => WelcomeScreen(),
-          LoginScreen.id: (context) => LoginScreen(),
-          RegScreen.id: (context) => RegScreen(),
         },
       ),
     );
